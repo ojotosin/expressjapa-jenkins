c6ef523de49c202cb6ae6680e16d77652564786e
Tosin Ojo <ojotosin1900@gmail.com>
2023-07-08 09:49:50 -0400

    updated the variables

--- docker/build_image-v2.sh
+++ docker/build_image-v2.sh
@@ -4,11 +4,11 @@
 docker build \
 --build-arg PERSONAL_ACCESS_TOKEN='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' \
 --build-arg GITHUB_USERNAME='ojotosin' \
---build-arg REPOSITORY_NAME='expressjapa-ecs'  \
---build-arg WEB_FILE_ZIP='travelfresh.zip' \
---build-arg WEB_FILE_UNZIP='travelfresh' \
+--build-arg REPOSITORY_NAME='expressjapa-jenkins'  \
+--build-arg WEB_FILE_ZIP='expressjapa.zip' \
+--build-arg WEB_FILE_UNZIP='expressjapa' \
 --build-arg RDS_DB_ENDPOINT='expressjapadb-v1.ckarc33mlfud.us-east-1.rds.amazonaws.com' \
 --build-arg RDS_DB_NAME='expressjapa_db_v1' \
 --build-arg RDS_MASTER_USERNAME='mysqluser' \
 --build-arg RDS_DB_PASSWORD='mysqlpassword' \
--t travelfreshv2 .    # tag name
+-t expressjapa .    # tag name
