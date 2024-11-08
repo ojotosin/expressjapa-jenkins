0fef90607951f5908ff9e269c540b5b8470e542e
Tosin Ojo <98328941+ojotosin@users.noreply.github.com>
2024-11-08 14:11:21 -0600

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
+# Initialize and destroy resources in each Terraform directory
+
 cd jenkins/terraform/agent
+terraform init -input=false
 terraform destroy --auto-approve
 
 cd ../acm-route53
+terraform init -input=false
 terraform destroy --auto-approve
 
 cd ../asg-lb
+terraform init -input=false
 terraform destroy --auto-approve
 
 cd ../efs
+terraform init -input=false
 terraform destroy --auto-approve
 
 cd ../iam
+terraform init -input=false
 terraform destroy --auto-approve
 
-# deregister the AMIs
+# Deregister the AMIs
 aws ec2 describe-images --filters "Name=name,Values=jenkins-controller,jenkins-agent" --query 'Images[*].ImageId' --output text | tr '\t' '\n' | xargs -I {} aws ec2 deregister-image --image-id {}
-
-# delete the parameter store values
-#aws ssm delete-parameter --name /devops-tools/jenkins/id_rsa
-#aws ssm delete-parameter --name /devops-tools/jenkins/id_rsa.pub
