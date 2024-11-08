3037e9d345a533c0b72abc2fc5c282f7c10a00d5
Tosin Ojo <98328941+ojotosin@users.noreply.github.com>
2024-11-08 09:41:08 -0600

    Update build_image-v2.sh

--- docker/build_image-v2.sh
+++ docker/build_image-v2.sh
@@ -3,13 +3,13 @@
 # Run the docker build command
 docker build \
 --no-cache \
---build-arg PERSONAL_ACCESS_TOKEN='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' \
+--build-arg PERSONAL_ACCESS_TOKEN='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' \
 --build-arg GITHUB_USERNAME='ojotosin' \
 --build-arg REPOSITORY_NAME='expressjapa-jenkins'  \
 --build-arg WEB_FILE_ZIP='expressjapa.zip' \
 --build-arg WEB_FILE_UNZIP='expressjapa' \
---build-arg RDS_DB_ENDPOINT='expressjapadb-v1.ckarc33mlfud.us-east-1.rds.amazonaws.com' \
---build-arg RDS_DB_NAME='expressjapa_db_v1' \
+--build-arg RDS_DB_ENDPOINT='expressjapadb-v1new.ckarc33mlfud.us-east-1.rds.amazonaws.com' \
+--build-arg RDS_DB_NAME='expressjapadb' \
 --build-arg RDS_MASTER_USERNAME='mysqluser' \
 --build-arg RDS_DB_PASSWORD='mysqlpassword' \
 -t expressjapa-v1 .    # tag name
